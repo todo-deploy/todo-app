4f66514671f7cf59f11e2932eb2b902486a1c2e8
Dan Siwiec <daniel.siwiec@gmail.com>
2019-03-15 00:09:12 -0700

    Change API url to be relative

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -7,7 +7,7 @@ var ESC_KEY = 27;
 $(function () {
 	'use strict';
 
-	app.todos.url = 'http://localhost:8080/api/'
+	app.todos.url = 'api/'
 	// kick things off by creating the `App`
 	new app.AppView();
 });
