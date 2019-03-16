d70abc0528c0124a6a745ea8c8d0ad722fa0f18e
Dan Siwiec <daniel.siwiec@gmail.com>
2019-03-15 20:29:51 -0700

    Address deprecations

--- app/server/controllers/db.js
+++ app/server/controllers/db.js
@@ -1,6 +1,8 @@
 mongoose = require('mongoose')
+mongoose.set('useCreateIndex', true)
+mongoose.set('useFindAndModify', false)
 
-mongoose.connect(process.env['DB'])
+mongoose.connect(process.env['DB'], { useNewUrlParser: true })
 mongoose.connection.on('open', () => console.log('Connected to db'))
 
 module.exports = mongoose
