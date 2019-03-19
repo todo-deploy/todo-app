ad9edad99b507aec6a8a0d8379f7be2e2d15bab7
Dan Siwiec <daniel.siwiec@gmail.com>
2019-03-18 22:41:59 -0700

    Add morgan for request logging

--- app/index.js
+++ app/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const routes = require('./server/app/routes')
 const port = process.env['PORT']
 
 const app = express()
+
+app.use(morgan('combined'))
 app.use(express.static(`${__dirname}/public`))
 app.use('/node_modules', express.static(`${__dirname}/../node_modules`))
 routes(app)
