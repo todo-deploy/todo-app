ed4e91f35958235006e30cc8f2c1d7eabd3e39cc
Dan Siwiec <daniel.siwiec@gmail.com>
2019-05-24 15:51:03 -0700

    Apply env variables for db user/pass

--- app/server/controllers/db.js
+++ app/server/controllers/db.js
@@ -2,7 +2,12 @@ mongoose = require('mongoose')
 mongoose.set('useCreateIndex', true)
 mongoose.set('useFindAndModify', false)
 
-mongoose.connect(process.env['DB'], { useNewUrlParser: true })
+mongoose.connect(process.env['DB'], {
+  user: process.env['DB_USER'],
+  pass: process.env['DB_PASS'],
+  dbName: process.env['DB_NAME'],
+  useNewUrlParser: true
+})
 mongoose.connection.on('open', () => console.log('Connected to db'))
 
 module.exports = mongoose
